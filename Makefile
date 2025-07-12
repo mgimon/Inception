e84f048a10ddf7215e2b0b64e3623cafe656e736
root <root@mgimon-c-vm.local>
2025-07-12 19:33:30 +0200

    mariadb container

--- Makefile
+++ Makefile
@@ -1,20 +1,17 @@
-# Makefile
-
 NAME=inception
 
 all: up
 
 up:
-	docker-compose -f srcs/docker-compose.yml up -d --build
+        docker-compose -f srcs/docker-compose.yml up -d --build
 
 down:
-	docker-compose -f srcs/docker-compose.yml down
+        docker-compose -f srcs/docker-compose.yml down
 
 clean: down
-	docker system prune -af --volumes
+        @docker system prune -af
 
 fclean: clean
-	docker volume rm $(docker volume ls -q)
-	docker rmi $(docker images -q)
+        @docker rmi $$(docker images -q) || true
 
 re: fclean all
--- srcs/requirements/mariadb/init_db.sh
+++ srcs/requirements/mariadb/init_db.sh
@@ -3,10 +3,11 @@
 # Inicializa daemon mysql
 mysqld --initialize --user=mysql
 
-# Arranca MySQL en segundo plano y espera a que esté listo
+# Arranca el daemon en modo seguro
 mysqld_safe --datadir="/var/lib/mysql" &
 sleep 5
 
+# Inserta instrucciones
 echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" > tmp.sql
 echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> tmp.sql
 echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';" >> tmp.sql
@@ -15,8 +16,8 @@ echo "FLUSH PRIVILEGES;" >> tmp.sql
 
 mysql < tmp.sql
 
-# Parar mysqld_safe después de setup
+# Desconecta el daemon
 mysqladmin shutdown -uroot -p"${MYSQL_ROOT_PASSWORD}"
 
-# Arranca mariadb en primer plano
+# Arranca el daemon de forma normal
 exec mysqld
